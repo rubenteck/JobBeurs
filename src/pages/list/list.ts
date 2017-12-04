6ed436e4487e37c474a27e519d19b4fbadf7e319
rubenteck <32707826+rubenteck@users.noreply.github.com>
2017-12-04 17:07:42 +0100

    lijst + symbolen

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class ListPage {
 
+  searchQuery: string = '';
+  items: string[];
+
   constructor(public navCtrl: NavController) {
-
+	this.initializeItems();
   }
+  
+  initializeItems() {
+	  
+	  //veranderen in lijst ophalen uit geheugen
+	  
+    this.items = [
+      'Amsterdam',
+      'Bogota'
+      
+    ];
+  }
+  
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
 
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
