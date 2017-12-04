f862c51ae3be41d78abd5287e49d10208d289a24
rubenteck <32707826+rubenteck@users.noreply.github.com>
2017-12-04 15:48:21 +0100

    2 paginas
    tabs aangepst, paginas en titels ook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { ListPage } from '../pages/list/list';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -14,8 +13,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ListPage,
     HomePage,
     TabsPage
   ],
@@ -26,8 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ListPage,
     HomePage,
     TabsPage
   ],
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-list',
+  templateUrl: 'list.html'
 })
-export class ContactPage {
+export class ListPage {
 
   constructor(public navCtrl: NavController) {
 
